39b948c25f075b52974d9aee2acf905a8fc78d0c
Saveree Joshipura <sjoshipu@uscd.edu>
2019-01-24 10:28:52 -0800

    finished lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -10,9 +11,27 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$(".jumbotron p").addClass("active");
+		$("#testjs").toggleClass("active");
 	});
+	$("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+
+function projectClick(e) { 
+	console.log("Project clicked");
+	e.preventDefault();
+	 var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+    	description.fadeOut()
+    	
+    //  	description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
